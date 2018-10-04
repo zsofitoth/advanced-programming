f111714747d6e8235cfb6062a6f119919b23cdc2
Zsofia Toth <tothzsozso@gmail.com>
2018-10-04 10:22:58 +0200

    added week 5

---
+++ 050-par/src/main/scala/adpro/Main.scala
@@ -0,0 +1,15 @@
+// Advanced Programming
+// Andrzej Wąsowski, IT University of Copenhagen
+
+package adpro
+import Par._
+
+
+//See "Main" as a Java-like "main" method. 
+object Main extends App {
+    
+    println("Welcome to Parallel Computation, the ADPRO 050 class!!")
+
+    // play with your functions below.  
+
+}
---
+++ 050-par/src/test/scala/adpro/Par_test.scala
@@ -0,0 +1,18 @@
+package adpro
+import org.scalatest.FunSuite
+import Par._
+
+class Stream_test extends FunSuite {
+  
+  test("This one always works: (-1) * (-1) = 1") {
+    assert((-1)*(-1)==1);
+  }
+  
+  //write  your tests below.
+
+	
+
+
+
+
+}
